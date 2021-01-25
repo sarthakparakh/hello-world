f2e02c1c6ec4a683f7d47e34ee9ce53d8b521e46
sarthak_parakh <sarthakparakh@gmail.com>
2021-01-25 14:59:12 +0530

    Added math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+def add(x,y):
+	return x+y;
+	
+def substract(x,y):
+    if x<y:
+        return ErrorMsg;
+    return x-y;
+	
+def divide(x,y):
+	return x*y;
+	
+def multiply(x,y):
+	if y==0:
+        return Divide_by_0_Error
+    return x/y;
+    
+def square(x):
+    return x*x;
+	
